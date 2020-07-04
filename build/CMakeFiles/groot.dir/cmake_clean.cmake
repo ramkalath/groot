f6926115078cd5248898f8ff89cf2013321b5810
ramkalath <ramkalath@gmail.com>
2020-07-04 23:37:06 +0530

    dummy commit

---
+++ build/CMakeFiles/groot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/groot"
+  "../bin/groot.pdb"
+  "CMakeFiles/groot.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/groot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*****************************************************************************
+* Filename : main.cpp
+* Date : 4-7-2020
+* Author : Ram
+* Email : ramkalath@gmail.com
+* Breif Description :
+* Detailed Description :
+*****************************************************************************/
+
+#include<iostream>
+
+int main()
+{
+	
+	return 0;
+}
